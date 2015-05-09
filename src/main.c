ebce2d1e389c73cd02f4329bd5f4d8a901e9a02c
Jim <black.belt.jimmy@gmail.com>
2015-05-09 15:45:47 -0700

    update demo programs to use new features

--- src/main.c
+++ src/main.c
@@ -26,7 +26,7 @@ can be found at www.yabasic.de
 /* ------------- defines ---------------- */
 
 #define DONE {current=current->next;break;}  /* reduces type-work */
-#define COPYRIGHT "original yabasic Copyright 1995-2006 by Marc-Oliver Ihm\n\tyab improvements Copyright 2006-2014 by Jan Bungeroth\n\tyab improvements Copyright 2013-2015 by Jim Saxton\n"
+#define COPYRIGHT "Original yabasic Copyright 1995-2006 by Marc-Oliver Ihm\n\tyab improvements Copyright 2006-2014 by Jan Bungeroth\n\tyab improvements Copyright 2013-2015 by Jim Saxton\n"
 #define BANNER \
 "\n        yab is yabasic for Haiku. This is version " VERSION ",\n       built on "\
 ARCHITECTURE " on " BUILD_TIME "\n\n      " COPYRIGHT "\n\n"
