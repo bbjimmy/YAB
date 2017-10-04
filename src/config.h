2b1e2a91778dbec2bad201bdddaad3ee9385418c
Jim Saxton <black.belt.jimmy@gmail.com>
2017-10-04 08:34:54 -0700

    prepare for release dur to BControlLook changes.

--- src/config.h
+++ src/config.h
@@ -5,7 +5,7 @@
 // #define LIBBSVG 
 #define BUILD_TIME __DATE__
 /* Version number of package */
-#define VERSION "1.7.5.4"
+#define VERSION "1.7.5.5"
 /* architecture of build machine */
 #define UNIX_ARCHITECTURE "BePC-Haiku"
 
